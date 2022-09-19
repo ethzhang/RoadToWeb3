17f2f5c0e4c27da3172a27b9882d5d6a7c164895
Ethan Zhang <ethan@EthandeMacBook-Pro.local>
2022-09-19 15:40:24 +0800

    add struct type to add more attributes

--- week03/ChainBattles/contracts/ChainBattles.sol
+++ week03/ChainBattles/contracts/ChainBattles.sol
@@ -7,24 +7,48 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
+// deploy to mumbai testnet, contract address is :0x8b3d36Fb0CdBbcAc1215f6Cf1f5FfE5c2Da6cC24
+
 contract ChainBattles is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping (uint256 => uint256) public tokenIdtoLevels;
+    uint256 MAX_SUPPLY = 10000;
+    uint    MAX_PER_WALLET = 5;
+
+    mapping(address => uint) public walletMints;
+
+    // mapping (uint256 => uint256) public tokenIdtoLevels;
+
+    mapping (uint256 => Hero) public tokenIdtoHero;
+
+    struct Hero {
+        uint256 level;
+        uint256 hp;
+        uint256 speed;
+        uint256 strength;
+    }
 
     constructor() ERC721("Chain Battles", "CBTLS") {
         
     }
 
     function generateCharacter(uint256 tokenId) public view returns (string memory) {
+        string memory level;
+        string memory hp;
+        string memory speed;
+        string memory strength;
+        (level, hp, speed, strength) = getHero(tokenId);
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
             '<rect width="100%" height="100%" fill="black" />',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
-            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ", level ,'</text>',
+            '<text x="50%" y="55%" class="base" dominant-baseline="middle" text-anchor="middle">', "HP: ", hp ,'</text>',
+            '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">', "Speed: ", speed ,'</text>',
+            '<text x="50%" y="65%" class="base" dominant-baseline="middle" text-anchor="middle">', "Strength: ", strength ,'</text>',
             '</svg>'
         );
         return string(
@@ -35,9 +59,14 @@ contract ChainBattles is ERC721URIStorage {
         );
     }
 
-    function getLevels(uint256 tokenId) public view returns (string memory){
-        uint256 levels = tokenIdtoLevels[tokenId];
-        return levels.toString();
+    // function getLevels(uint256 tokenId) public view returns (string memory){
+    //     uint256 levels = tokenIdtoLevels[tokenId];
+    //     return levels.toString();
+    // }
+    
+    function getHero(uint256 tokenId) public view returns (string memory,  string memory, string memory, string memory) {
+        Hero storage hero = tokenIdtoHero[tokenId];
+        return (hero.level.toString(), hero.hp.toString(), hero.speed.toString(),hero.strength.toString());
     }
 
     function getTokenURI(uint256 tokenId) public view returns (string memory) {
@@ -60,7 +89,11 @@ contract ChainBattles is ERC721URIStorage {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdtoLevels[newItemId] = 0;
+
+        Hero memory hero = Hero(1, 100, 10, 25);
+        tokenIdtoHero[newItemId] = hero;
+
+        // tokenIdtoLevels[newItemId] = 0;
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
@@ -68,8 +101,16 @@ contract ChainBattles is ERC721URIStorage {
         require(_exists(tokenId), "Please use an existing token");
         require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
 
-        uint256 currentLevel = tokenIdtoLevels[tokenId];
-        tokenIdtoLevels[tokenId] = currentLevel + 1;
+        Hero storage hero = tokenIdtoHero[tokenId];
+        hero.level = hero.level + 1;
+        hero.hp = hero.hp + 100;
+        hero.speed = hero.speed + 10;
+        hero.strength = hero.strength + 25;
+        tokenIdtoHero[tokenId] = hero;
+
+        // uint256 currentLevel = tokenIdtoLevels[tokenId];
+        // tokenIdtoLevels[tokenId] = currentLevel + 1;
+
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
