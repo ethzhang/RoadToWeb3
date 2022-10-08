7b206770cad3d8107f4fd695bc21ad835acf5679
Ethan Zhang <ethan@EthandeMacBook-Pro.local>
2022-10-08 14:47:24 +0800

    generate a random index to choose a random image for the NFTs

--- week05/contracts/MockPriceFeed.sol
+++ week05/contracts/MockPriceFeed.sol
@@ -4,3 +4,5 @@ pragma solidity ^0.7.0;
 import "@chainlink/contracts/src/v0.7/tests/MockV3Aggregator.sol";
 
 // This file create a MockAggregator (price feed smart contract) for testing.
+
+// Deployed to Goerli, Address :0xCa8bAf433a8Bb2E4D0D5E46bCBC732929871F84E
